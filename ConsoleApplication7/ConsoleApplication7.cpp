9ff6e261013fca0cf09ca6854da58b75a286e530
Atos <70628905+ireadyium@users.noreply.github.com>
2023-10-06 07:54:36 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a;
+	int b;
+	int c;
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	if ((a > 0 && b <= 0 && c <= 0) || (a <= 0 && b > 0 && c <= 0) || (a <= 0 && b <= 0 && c > 0)) {
+		std::cout << "Истина";
+	}
+	else {
+		std::cout << "Ложь";
+	}
+}
